21fb5167bf4ff00290c1b02f1409c117c2b35510
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2018-03-24 01:07:03 +0100

    Added start script.

---
+++ start-cHackPanelControl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ARGUMENTS="C_Hack"
+cd $(dirname $(realpath $0))
+
+git remote update
+ret=$(git status -suno)
+if [ ${#ret} -gt 2 ] ;then
+  git pull
+  mvn package
+fi
+
+biggest="0.0.0"
+FILES=target/C-Hack-Panel-Control-*-jar-with-dependencies.jar
+for f in $FILES
+do
+  currVer=${f#*-Control-}
+  currVer=${currVer%-jar-*}
+  biggestNum=${biggest//./0}
+  currNum=${currVer//./0}
+
+  if [ $currNum -gt $biggestNum ] ;then
+    biggest=$currVer
+  fi
+
+done
+
+echo $biggest
+
+java -jar target/C-Hack-Panel-Control-$biggest-jar-with-dependencies.jar $ARGUMENTS
+
