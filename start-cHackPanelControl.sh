c15dd0b927f9efda772ead7e8deaccd1d10c9aeb
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2018-03-24 01:10:57 +0100

    Changed start script

--- start-cHackPanelControl.sh
+++ start-cHackPanelControl.sh
@@ -3,8 +3,8 @@ ARGUMENTS="C_Hack"
 cd $(dirname $(realpath $0))
 
 git remote update
-ret=$(git status -suno)
-if [ ${#ret} -gt 2 ] ;then
+ret=$(git status -uno|grep "Ihr Branch ist auf dem selben Stand wie 'origin/master'")
+if [ ${#ret} -lt 2 ] ;then
   git pull
   mvn package
 fi
