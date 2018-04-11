92739537c705060aa7fb8aadfac9f623224d492b
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2018-04-11 21:10:15 +0200

    Second try at fixing start script.

--- updateAndStart.sh
+++ updateAndStart.sh
@@ -7,11 +7,12 @@ cd $(dirname $(realpath $0))
 
 ./update.sh
 
+if [ ! -e target ] ;then mvn package ;fi
+
 biggest="0.0.0"
 FILES=target/C-Hack-Panel-Control-*-jar-with-dependencies.jar
 for f in $FILES
 do
-  if [ "$f" == "$FILES" ] ;then continue ;fi
   currVer=${f#*-Control-}
   currVer=${currVer%-jar-*}
   biggestNum=${biggest//./0}
