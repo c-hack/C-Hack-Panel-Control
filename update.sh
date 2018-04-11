97d3928b06f1e66b1d31b02fb46a700fcb655a07
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2018-04-11 21:02:34 +0200

    Changed start script and added standalone update script.

---
+++ update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd $(dirname $(realpath $0))
+
+git remote update
+ret=$(git status -uno|grep "Ihr Branch ist auf dem selben Stand wie 'origin/master'")
+if [ ${#ret} -lt 2 ] ;then
+  git pull
+  mvn package
+fi
--- updateAndStart.sh
+++ updateAndStart.sh
@@ -5,12 +5,7 @@ BAUD="115200"
 
 cd $(dirname $(realpath $0))
 
-git remote update
-ret=$(git status -uno|grep "Ihr Branch ist auf dem selben Stand wie 'origin/master'")
-if [ ${#ret} -lt 2 ] ;then
-  git pull
-  mvn package
-fi
+./update.sh
 
 biggest="0.0.0"
 FILES=target/C-Hack-Panel-Control-*-jar-with-dependencies.jar
